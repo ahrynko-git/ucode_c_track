f0e83d4ab40a62087a8b777c0eaadad63958bbf0
Andrii Hrynko <ahrynko@e2r13p8.unit.ua>
2020-01-06 21:44:51 +0200

    change mx_memcmp

--- libmx/src/mx_memcmp.c
+++ libmx/src/mx_memcmp.c
@@ -6,7 +6,7 @@ int mx_memcmp(const void *s1, const void *s2, size_t n) {
 
     if (n == 0)
         return 0;
-    for (; n && *p_s1 == *p_s2 && *p_s1 && *p_s2; n--, p_s1++, p_s2++);
+    for (; n && *p_s1 == *p_s2; n--, p_s1++, p_s2++);
     return *p_s1 - *p_s2;
 }
 
